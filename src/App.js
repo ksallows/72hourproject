c01dd87c1e728fdcc32c80a26663c31dab6fe4c5
Katie Sallows <ktsallows@gmail.com>
2021-11-18 18:42:21 -0500

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test branch c1
+          Test branch Katie
         </p>
         <a
           className="App-link"
